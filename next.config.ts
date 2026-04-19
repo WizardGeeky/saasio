033406b70bfc81d7140262779d6ba6850b9444c5
eswararao.beta <eswararao.beta@toucanus.com>
2026-04-19 08:56:02 +0530

    Bug is fixed: while generating pdf with Ai

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
+  outputFileTracingIncludes: {
+    "/api/v1/private/ai-ats": [
+      "./node_modules/pdfjs-dist/legacy/build/pdf.worker.mjs",
+      "./node_modules/pdf-parse/node_modules/pdfjs-dist/legacy/build/pdf.worker.mjs",
+    ],
+    "/api/v1/private/resume-ai": [
+      "./node_modules/pdfjs-dist/legacy/build/pdf.worker.mjs",
+      "./node_modules/pdf-parse/node_modules/pdfjs-dist/legacy/build/pdf.worker.mjs",
+    ],
+  },
   serverExternalPackages: ["pdf-parse", "pdfjs-dist"],
 };
 
